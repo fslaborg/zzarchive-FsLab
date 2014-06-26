c6ca775bc198c56d41bc367a98733f52e0649c5a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2014-06-26 10:00:47 +0200

    Always use latest released FAKE

--- build.cmd
+++ build.cmd
@@ -1,7 +1,6 @@
 @echo off
 cls
-if not exist packages\FAKE\tools\Fake.exe ( 
-  .nuget\nuget.exe install FAKE -OutputDirectory packages -Version 3.0.0-alpha7 -ExcludeVersion  
-)
+.nuget\nuget.exe install FAKE -OutputDirectory packages -ExcludeVersion  
+
 packages\FAKE\tools\FAKE.exe build.fsx %*
 pause
