b04569d0f78e9f92838073118446cd4a4978d11d
Evelina Gabasova <evelina@evelinag.com>
2015-02-10 16:03:03 +0100

    Call FsLab Journal process with specified path
    Call FsLab Journal process with specified path where to look for script files to process

--- src/journal/Main.fs
+++ src/journal/Main.fs
@@ -8,9 +8,21 @@ let main args =
   //
   //  --latex              Generate output as LaTeX rather than the default HTML
   //  --non-interactive    Do not open the generated HTML document in web browser
+  //  --path name          Root path where to look for script files to process
   //
   let latex = args |> Seq.exists ((=) "--latex")
   let browse = args |> Seq.exists ((=) "--non-interactive") |> not
-  if latex then Journal.Process(outputKind = OutputKind.Latex)
-  else Journal.Process(browse)
+  let path = 
+    let idx = args |> Seq.tryFindIndex ((=) "--path")
+    match idx with
+    | None -> None
+    | Some(i) -> Some args.[i+1]    // return next argument
+
+  match path with
+  | None -> 
+      if latex then Journal.Process(outputKind = OutputKind.Latex)
+      else Journal.Process(browse)
+  | Some(root) ->
+      if latex then Journal.Process(root = root, outputKind = OutputKind.Latex)
+      else Journal.Process(root = root, browse = browse)
   0
