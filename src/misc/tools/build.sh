fc3588c1f6bb649dc1ebbb9c31e5399637120ab8
Tomas Petricek <info@tomasp.net>
2015-04-24 00:35:00 +0100

    Support quickrun in bash script

--- src/misc/tools/build.sh
+++ src/misc/tools/build.sh
@@ -2,32 +2,38 @@
 if test "$OS" = "Windows_NT"
 then
   # use .Net
-
-  .paket/paket.bootstrapper.exe
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
+  if test "$1" = "quickrun" 
+  then
+    packages/FAKE/tools/FAKE.exe run --fsiargs -d:NO_FSI_ADDPRINTER build.fsx
+  else
+    .paket/paket.bootstrapper.exe
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+      exit $exit_code
+    fi
+    .paket/paket.exe restore
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+  	  exit $exit_code
+    fi
+    packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:NO_FSI_ADDPRINTER build.fsx
   fi
-
-  .paket/paket.exe restore
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-
-  packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:NO_FSI_ADDPRINTER build.fsx
 else
   # use mono
-  mono .paket/paket.bootstrapper.exe prerelease
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
+  if test "$1" = "quickrun" 
+  then
+    mono packages/FAKE/tools/FAKE.exe run --fsiargs -d:NO_FSI_ADDPRINTER -d:MONO build.fsx
+  else
+    mono .paket/paket.bootstrapper.exe
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+      exit $exit_code
+    fi
+    mono .paket/paket.exe restore
+    exit_code=$?
+    if [ $exit_code -ne 0 ]; then
+  	  exit $exit_code
+    fi
+    mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:NO_FSI_ADDPRINTER -d:MONO build.fsx 
   fi
-
-  mono .paket/paket.exe restore
-  exit_code=$?
-  if [ $exit_code -ne 0 ]; then
-  	exit $exit_code
-  fi
-  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:NO_FSI_ADDPRINTER -d:MONO build.fsx 
 fi
