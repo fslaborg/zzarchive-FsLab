a6fb307158c101c8dcf735447391b77baf8147fd
tpetricek <tomas@tomasp.net>
2015-04-24 03:56:07 +0100

    Install on first build (Windows)

--- build.cmd
+++ build.cmd
@@ -2,13 +2,17 @@
 cls
 
 if "%1" == "quickrun" (
-  packages\FAKE\tools\FAKE.exe run --fsiargs -d:NO_FSI_ADDPRINTER build.fsx 
+  packages\FAKE\tools\FAKE.exe run --fsiargs -d:NO_FSI_ADDPRINTER build.fsx
 ) else (
   .paket\paket.bootstrapper.exe
   if errorlevel 1 (
     exit /b %errorlevel%
   )
-  .paket\paket.exe restore
+  if not exist paket.lock (
+    .paket\paket.exe install
+  ) else (
+    .paket\paket.exe restore
+  )
   if errorlevel 1 (
     exit /b %errorlevel%
   )
