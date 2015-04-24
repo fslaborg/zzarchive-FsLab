7060b08a2ca4d41407018315bc6461301037a22b
tpetricek <tomas@tomasp.net>
2015-04-24 01:35:25 +0100

    Comment the FsLab runner

--- src/journal/Runner.fs
+++ src/journal/Runner.fs
@@ -1,7 +1,7 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
-
-
+﻿// --------------------------------------------------------------------------------------
+// Runner for processing FsLab Journals - this gets executed when you run the project.
+// All work is deleagetd to "build.fsx" FAKE script - we just run it using cmd/bash.
+// --------------------------------------------------------------------------------------
 open System.IO
 open System.Diagnostics
 
@@ -10,17 +10,22 @@ let main argv =
   let (@@) p1 p2 = Path.Combine(p1, p2)
   let currentDir = __SOURCE_DIRECTORY__
 
+  // If the build file is empty (project was just created)
+  // we copy the latest version from the FsLab.Runner package
   if File.ReadAllBytes(currentDir @@ "build.fsx").Length < 10 then
-    //
     for f in Directory.GetFiles(currentDir @@ "packages/FsLab.Runner/tools") do
       File.Copy(f, currentDir @@ Path.GetFileName(f), true)
 
+  // Start the build script using the appropriate command
   let info = 
     if System.Type.GetType("Mono.Runtime") <> null then
-      new ProcessStartInfo("bash", "build.sh quickrun", UseShellExecute = false, WorkingDirectory = currentDir)
+      new ProcessStartInfo("bash", "build.sh quickrun")
     else
-      new ProcessStartInfo("cmd", "/c build.cmd quickrun", UseShellExecute = false, WorkingDirectory = currentDir)
+      new ProcessStartInfo("cmd", "/c build.cmd quickrun")
+  info.UseShellExecute <- false
+  info.WorkingDirectory <- currentDir
 
+  // Run the process and wait for it to complete
   let proc = Process.Start(info)
   proc.WaitForExit()
   proc.ExitCode
