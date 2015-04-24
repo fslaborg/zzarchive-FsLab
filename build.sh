017c1326ebcff56f7d132ee0c1f2b6da556439d8
Tomas Petricek <tomas@tomasp.net>
2015-04-24 04:01:23 +0100

    Install on first build and simplify script

--- build.sh
+++ build.sh
@@ -1,39 +1,29 @@
 #!/bin/bash
 if test "$OS" = "Windows_NT"
 then
-  # use .Net
-  if test "$1" = "quickrun" 
-  then
-    packages/FAKE/tools/FAKE.exe run --fsiargs -d:NO_FSI_ADDPRINTER build.fsx
-  else
-    .paket/paket.bootstrapper.exe
-    exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-      exit $exit_code
-    fi
-    .paket/paket.exe restore
-    exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-  	  exit $exit_code
-    fi
-    packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:NO_FSI_ADDPRINTER build.fsx
-  fi
+  MONO=""
 else
-  # use mono
-  if test "$1" = "quickrun" 
-  then
-    mono packages/FAKE/tools/FAKE.exe run --fsiargs -d:NO_FSI_ADDPRINTER -d:MONO build.fsx
-  else
-    mono .paket/paket.bootstrapper.exe
-    exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-      exit $exit_code
-    fi
-    mono .paket/paket.exe restore
-    exit_code=$?
-    if [ $exit_code -ne 0 ]; then
-  	  exit $exit_code
-    fi
-    mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:NO_FSI_ADDPRINTER -d:MONO build.fsx 
+  MONO="mono"
+fi
+
+if test "$1" = "quickrun"
+then
+  $MONO packages/FAKE/tools/FAKE.exe run --fsiargs -d:NO_FSI_ADDPRINTER build.fsx
+else
+  $MONO .paket/paket.bootstrapper.exe
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+    exit $exit_code
   fi
+  if [ -e "paket.lock" ]
+  then
+    $MONO .paket/paket.exe restore
+  else
+    $MONO .paket/paket.exe install
+  fi
+  exit_code=$?
+  if [ $exit_code -ne 0 ]; then
+	  exit $exit_code
+  fi
+  $MONO packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:NO_FSI_ADDPRINTER build.fsx
 fi
