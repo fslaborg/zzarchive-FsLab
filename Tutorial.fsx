e2a97f7133c2737e9f6d842d2991d7ce34741b4e
Tomas Petricek <info@tomasp.net>
2016-07-06 13:16:31 +0100

    Update tutorial for new FsLab

--- Tutorial.fsx
+++ Tutorial.fsx
@@ -1,12 +1,12 @@
-﻿// Before running any code, invoke Paket to get the dependencies. 
+// Before running any code, invoke Paket to get the dependencies. 
 //
-// You can either build the project (Ctrl + Alt + B in VS) or run 
-// '.paket/paket.bootstrap.exe' and then '.paket/paket.exe install'
-// (if you are on a Mac or Linux, run the 'exe' files using 'mono')
+// To do this, build the project or run '.paket/paket.bootstrap.exe' and
+// then '.paket/paket.exe install' (on Mac run 'exe' files using 'mono').
 //
-// Once you have packages, use Alt+Enter (in VS) or Ctrl+Enter to
-// run the following in F# Interactive. You can ignore the project
-// (running it doesn't do anything, it just contains this script)
+// Once you have packages, use Alt+Enter or Ctrl+Enter to run the following
+// snippets. In Ionide, outputs will be embedded in the F# Interactive window
+// (and you can choose "theme" to match with your editor color settings).
+#load "packages/FsLab/Themes/DefaultWhite.fsx"
 #load "packages/FsLab/FsLab.fsx"
 
 open Deedle
