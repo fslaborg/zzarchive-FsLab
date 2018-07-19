3741f32b40dca4eb5927b2d6a3359ebeef833ae1
Don Syme <dsyme@microsoft.com>
2018-07-19 15:11:38 +0100

    try / instead

--- build.fsx
+++ build.fsx
@@ -170,13 +170,13 @@ Target "UpdateNuSpec" (fun _ ->
     let doc = XDocument.Load(path)
     let files = doc.Descendants(XName.Get "files").First()
     files.RemoveAll()
-    files.Add(XElement(!"file", XAttribute(!"src", "..\\temp\\FsLab.fsx"), XAttribute(!"target", ".")))
+    files.Add(XElement(!"file", XAttribute(!"src", "../temp/FsLab.fsx"), XAttribute(!"target", ".")))
     let includes =
       [ "temp/Shared"; "temp/Text"; "temp/Html"; "temp/Themes" ]
       |> Seq.collect Directory.GetFiles
     for f in includes do
       let subdir = Path.GetDirectoryName(f).Substring(5)
-      files.Add(XElement(!"file", XAttribute(!"src", "..\\" + f.Replace("/", "\\")), XAttribute(!"target", subdir)))
+      files.Add(XElement(!"file", XAttribute(!"src", "../" + f), XAttribute(!"target", subdir)))
     doc.Save(path + ".updated")
     DeleteFile path
     Rename path (path + ".updated")
